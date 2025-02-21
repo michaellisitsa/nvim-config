f0460cd89a5238876d25e69919451abdfc1a6355
Michael Lisitsa <michael.lisitsa@gmail.com>
2025-02-22 09:04:04 +1100

    First commit

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,26 @@
+-- return {
+--   -- add nightfox
+--   { "EdenEast/nightfox.nvim" },
+--
+--   -- Configure LazyVim to load nightfox
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "nightfox",
+--     },
+--   },
+-- }
+
+return {
+  -- add vscode theme
+  --https://github.com/Mofiqul/vscode.nvim
+  { "Mofiqul/vscode.nvim" },
+
+  -- Configure LazyVim to load nightfox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "vscode",
+    },
+  },
+}
